4d2928d4b09cc4817bc806c1fc7b749f8773c360
lhw1357 <lhw1357@naver.com>
2017-06-08 01:08:59 +0900

    creating xmls

---
+++ app/src/main/java/com/example/user/thingstodo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.user.thingstodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    FloatingActionButton add;
+    Toolbar toolbar;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        add = (FloatingActionButton) findViewById(R.id.add);
+        setSupportActionBar(toolbar);
+
+        add.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent addthings = new Intent(this,addthings.class);
+        switch (v.getId()){
+            case R.id.add :
+
+                startActivity(addthings);
+
+                break;
+
+        }
+
+    }
+}
